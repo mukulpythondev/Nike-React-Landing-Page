8b2c4779e63160150d2d5ed6107afe41fce79598
Mukul Rana <134795697+mukulpythondev@users.noreply.github.com>
2023-09-08 08:22:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
+  plugins: [],
+}
+
